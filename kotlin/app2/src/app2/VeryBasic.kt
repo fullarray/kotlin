9048e783c1c32f9514080b2696b9970eee0789b3
fullarray <sifreed77@gmail.com>
2020-05-15 23:18:43 -0400

    Added app2 and enhanced it

---
+++ kotlin/app2/src/app2/VeryBasic.kt
@@ -0,0 +1,7 @@
+package app1
+class VeryBasic {
+	
+}
+fun main(args: Array<String>){
+	val basic: VeryBasic = VeryBasic()
+}
---
+++ kotlin/app2/src/app2/abstractBakeryGood.kt
@@ -0,0 +1,40 @@
+/**
+ *
+ * Author: fullarray
+ * Description: adapted
+ *
+ */
+
+
+package app2
+
+abstract class abstractBakeryGood(val flavour: String) {
+	fun eat(): String{
+		return "nom, nom, nom, nom,... delicious $flavour ${name()}"
+	}
+	
+	/**fun name(): String{
+		return "bakery good"
+	}*/
+	
+	abstract fun name(): String
+}
+
+class Customer(val name: String){
+	fun eats(food: abstractBakeryGood){
+		println("$name is eating... ${food.eat()} on that bench")
+	}
+}
+
+open class Donut(flavour: String, val topping: String): abstractBakeryGood(flavour){
+	override fun name(): String{
+		return "donut with $topping topping"
+	}
+}
+
+
+fun main(args: Array<String>){
+	val myDonut = Donut("Custard", "Powdered sugar")
+	val mario = Customer("Mario")
+	mario.eats(myDonut)
+}
